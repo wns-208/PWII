0c2a7914b9ff969a4aca8f6169a28034ce35c704
wns-208 <lordwillias@gmail>
2025-04-01 16:05:25 -0300

    ghast

---
+++ EXERCÍCIO 2.0 - Vetor, Matriz, Array/ex1 .php
@@ -0,0 +1,40 @@
+<?php
+
+    $alunos[
+        [
+        'nome' => 'osvado',
+        'rm' => '1233235', 
+        'telefone' => '999992321223' ,
+        'foto' => '',
+       
+    ],
+    [
+
+        'nome' => 'herique', 
+        'rm' => '54543645',
+        'foto' => '',
+        'telefone' => '767454354',
+    ],
+    [
+        'nome' => 'estevao',
+        'rm' => '5555455',
+        'foto' => '',
+        'telefone' => '67676767642',
+    ],
+    [
+        'nome' => 'pombo',
+        'rm' => '343232323',
+        'foto' => '',
+        'telefone' z=> '3424223332',
+    ],
+
+    ];
+
+    foreach ($alunos as $key => $value) {
+      echo 'o aluno'. $alunos['nome'];
+
+    };
+
+
+
+?>
