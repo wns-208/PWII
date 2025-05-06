508128daff0cbc48159e5eca8b92e48b999b5a9d
wns-208 <lordwillias@gmail.com>
2025-05-06 16:26:20 -0300

    xcx cs

---
+++ PDO_CRUD/public/usuario/read.php
@@ -0,0 +1,18 @@
+<?php 
+include '../../config/connection.php';
+
+$stmt = $pdo->query('SELECT * FROM usuario');
+$usuarios = $stmt->fetchAll();
+
+// echo"<pre>";
+// echo var_dump($usuarios);
+
+?>
+
+<?php foreach ($usuarios as $indice => $user) { ?>
+    <p>
+    <strong>Nome de Heroina:</stong><?php echo $user['username']; ?>
+   </p>
+   <hr>
+<?php } ?>
+
